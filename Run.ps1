fe4f5747cf8dd2f21ab474fcbe8919628654df1d
Srinivasa-Prasad <40587782+Srinivasa-Prasad@users.noreply.github.com>
2018-12-07 11:15:37 +0530

    Update Run.ps1

--- Run.ps1
+++ Run.ps1
@@ -1,3 +1,8 @@
 ## Download Latest Docker Installer (Windows) from Docker Website.
 "$(Get-Date) - Information - Initiating Docker For Windows Installer Download."| Out-File C:\Configure.log -Append
 Invoke-WebRequest 'https://download.docker.com/win/stable/Docker for Windows Installer.exe' -OutFile 'C:\Docker for Windows Installer.exe'
+"$(Get-Date) - Information - Download Completed."| Out-File C:\Configure.log -Append
+
+## Install Hyper-V Feature required for Docker Installation.
+"$(Get-Date) - Information - Initiating Hyper-V Feature installation."| Out-File C:\Configure.log -Append
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
