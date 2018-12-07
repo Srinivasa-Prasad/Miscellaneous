ceea2c4a400fe1d04debffa71e5080057136ba17
Srinivasa-Prasad <40587782+Srinivasa-Prasad@users.noreply.github.com>
2018-12-07 14:23:38 +0530

    Update Run.ps1

--- Run.ps1
+++ Run.ps1
@@ -5,10 +5,14 @@ Invoke-WebRequest 'https://download.docker.com/win/stable/Docker for Windows Ins
 
 ## Install Hyper-V Feature required for Docker Installation.
 "$(Get-Date) - Information - Initiating Hyper-V Feature installation."| Out-File C:\Configure.log -Append
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 "$(Get-Date) - Information - Download Completed."| Out-File C:\Configure.log -Append
 
 ## Install Docker for Windows
 "$(Get-Date) - Information - Initiating Docker for Windows installation."| Out-File C:\Configure.log -Append
-## "C:\Docker for Windows Installer.exe" install --quiet
+"C:\Docker for Windows Installer.exe" install --quiet
 "$(Get-Date) - Information - Docker for Windows installation completed."| Out-File C:\Configure.log -Append
+
+## Finally after all configuration. Restart Computer.
+## CustomScriptExtension shouldn't be used for restarting computer. However this script is verified and tested OK.
+Restart-Computer -Force
