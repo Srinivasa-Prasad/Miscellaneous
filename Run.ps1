1234666a0673f3d24d1e44f1bb580a1e4b90b136
Srinivasa-Prasad <40587782+Srinivasa-Prasad@users.noreply.github.com>
2018-12-07 11:02:17 +0530

    Update Run.ps1

--- Run.ps1
+++ Run.ps1
@@ -1,1 +1,3 @@
-"I got executed.!!! $(Get-Date)"| Out-File C:\Execute.log -Append
+## Download Latest Docker Installer (Windows) from Docker Website.
+"$(Get-Date) - Information - Initiating Docker For Windows Installer Download."| Out-File C:\Configure.log -Append
+Invoke-WebRequest 'https://download.docker.com/win/stable/Docker for Windows Installer.exe' -OutFile 'C:\Docker for Windows Installer.exe'
