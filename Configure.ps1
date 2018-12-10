d973b2818aa6766ade1c315abc5b51cd11af83fa
Srinivasa-Prasad <40587782+Srinivasa-Prasad@users.noreply.github.com>
2018-12-10 13:34:08 +0530

    Update Configure.ps1

--- Configure.ps1
+++ Configure.ps1
@@ -1,10 +1,12 @@
-## Configure Visual Studio Code Installation.
-"$(Get-Date) - Information - Initiating PowerShell DSC Configuration."| Out-File C:\DSCConfigure.log -Append
-
-"$(Get-Date) - Information - Initiating Visual Studio Code Installation."| Out-File C:\DSCConfigure.log -Append
-$EXE = "C:\VSCodeSetup.exe"
-$Arguments = "/VERYSILENT /MERGETASKS=!runcode"
-
-Start-Process $EXE $Arguments -Wait
-
-"$(Get-Date) - Information - Completed Visual Studio Code Installation.."| Out-File C:\DSCConfigure.log -Append
+Function fnConfigure(){
+    ## Configure Visual Studio Code Installation.
+    "$(Get-Date) - Information - Initiating PowerShell DSC Configuration."| Out-File C:\DSCConfigure.log -Append
+    
+    "$(Get-Date) - Information - Initiating Visual Studio Code Installation."| Out-File C:\DSCConfigure.log -Append
+    $EXE = "C:\VSCodeSetup.exe"
+    $Arguments = "/VERYSILENT /MERGETASKS=!runcode"
+    
+    Start-Process $EXE $Arguments -Wait
+    
+    "$(Get-Date) - Information - Completed Visual Studio Code Installation.."| Out-File C:\DSCConfigure.log -Append
+}
