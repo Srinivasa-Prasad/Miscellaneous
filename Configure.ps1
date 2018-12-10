5943e849996605a3b60b8c67fedb756785e9898d
Srinivasa-Prasad <40587782+Srinivasa-Prasad@users.noreply.github.com>
2018-12-10 13:21:16 +0530

    Update Configure.ps1

--- Configure.ps1
+++ Configure.ps1
@@ -1,2 +1,10 @@
 ## Download Latest Docker Installer (Windows) from Docker Website.
 "$(Get-Date) - Information - Initiating PowerShell DSC Configuration."| Out-File C:\DSCConfigure.log -Append
+
+"$(Get-Date) - Information - Initiating Visual Studio Code Installation."| Out-File C:\DSCConfigure.log -Append
+$EXE = "C:\VSCodeSetup.exe"
+$Arguments = "/VERYSILENT /MERGETASKS=!runcode"
+
+Start-Process $EXE $Arguments -Wait
+
+"$(Get-Date) - Information - Completed Visual Studio Code Installation.."| Out-File C:\DSCConfigure.log -Append
