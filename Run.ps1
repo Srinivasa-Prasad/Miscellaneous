370251d46c97bde1b8c06d072c1aec5b67c57700
Srinivasa-Prasad <40587782+Srinivasa-Prasad@users.noreply.github.com>
2018-12-12 09:14:16 +0530

    Update Run.ps1
    ## Install Hyper-V Feature required for Docker Installation.
    ## Download Docker for Windows.
    ## Download Latest Visual Studio Code Installer.
    ## Download Latest Git Installer.
    ## Docker for Windows Installation.
    ## Visual Studio Code Installation.
    ## Git Installation.
    ## Download AzureRm PowerShell Modules.
    ## Finally after all configuration. Restart Computer.

--- Run.ps1
+++ Run.ps1
@@ -1,9 +1,14 @@
 ## Install Hyper-V Feature required for Docker Installation.
 "$(Get-Date) - Information - Initiating Hyper-V Feature installation."| Out-File C:\Configure.log -Append
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
-"$(Get-Date) - Information - Download Completed."| Out-File C:\Configure.log -Append
+"$(Get-Date) - Information - Hyper-V Feature installation completed."| Out-File C:\Configure.log -Append
 
-## Download Latest Visual Studio Code Installer..
+## Download Docker for Windows.
+"$(Get-Date) - Information - Initiating Docker For Windows Installer download."| Out-File C:\Configure.log -Append
+Invoke-WebRequest 'https://download.docker.com/win/stable/Docker for Windows Installer.exe' -OutFile 'C:\Docker for Windows Installer.exe'
+"$(Get-Date) - Information - Docker for Windows download completed."| Out-File C:\Configure.log -Append
+
+## Download Latest Visual Studio Code Installer.
 "$(Get-Date) - Information - Initiating Visual Studio Code Download."| Out-File C:\Configure.log -Append
 Invoke-WebRequest 'https://srinivasablobstorage.file.core.windows.net/downloads/VSCodeSetup-x64-1.29.1.exe?st=2018-12-10T04%3A04%3A40Z&se=2020-01-01T04%3A04%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=HcgbJvJkyCkrdRWSw57cZdgtkG40u70tt92EHyux374%3D' -OutFile 'C:\VSCodeSetup.exe'
 "$(Get-Date) - Information - Visual Studio Code Download Completed."| Out-File C:\Configure.log -Append
@@ -13,7 +18,16 @@ Invoke-WebRequest 'https://srinivasablobstorage.file.core.windows.net/downloads/
 Invoke-WebRequest 'https://srinivasablobstorage.file.core.windows.net/downloads/Git-2.19.1-64-bit.exe?st=2018-12-11T23%3A42%3A12Z&se=2020-12-30T23%3A42%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=NIGD9JpQw1U2L5GbTSHoFGpEs4jg8Fzwhgg7kvxDHE8%3D' -OutFile 'C:\Git.exe'
 "$(Get-Date) - Information - Git Download Completed."| Out-File C:\Configure.log -Append
 
-## Visual Studio Code Installation.   
+## Docker for Windows Installation. 
+"$(Get-Date) - Information - Initiating Docker Installation."| Out-File C:\Configure.log -Append
+$EXE = "C:\Docker for Windows Installer.exe"
+$Arguments = "install --quiet"
+    
+Start-Process $EXE $Arguments -Wait
+    
+"$(Get-Date) - Information - Completed Docker Installation.."| Out-File C:\Configure.log -Append
+
+## Visual Studio Code Installation. 
 "$(Get-Date) - Information - Initiating Visual Studio Code Installation."| Out-File C:\Configure.log -Append
 $EXE = "C:\VSCodeSetup.exe"
 $Arguments = "/VERYSILENT /MERGETASKS=!runcode"
@@ -22,7 +36,7 @@ Start-Process $EXE $Arguments -Wait
     
 "$(Get-Date) - Information - Completed Visual Studio Code Installation.."| Out-File C:\Configure.log -Append
 
-## Git Installation.   
+## Git Installation.
 "$(Get-Date) - Information - Initiating Git Installation."| Out-File C:\Configure.log -Append
 $EXE = "C:\Git.exe"
 $Arguments = '/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"'
