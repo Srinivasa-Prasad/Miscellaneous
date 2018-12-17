701bc7bcf50d203c51637280419be278c8e4c277
Srinivasa <srinivasa.prasad@anz.com>
2018-12-17 15:04:36 +0530

    Software Folder

--- AzureDeploy.ps1
+++ AzureDeploy.ps1
@@ -9,5 +9,8 @@ If((Get-AzureRmContext).Account.Id -ne 'srinivasa.prasad@hotmail.com'){
 ## New-AzureRmResourceGroupDeployment -Name ARMInfraDeploy -ResourceGroupName SrinivasaLabs-Infra -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployInfra.json
 
 ## Deploy VM
-New-AzureRmResourceGroup -Name PRASASRI -Location 'East US' -Force
-New-AzureRmResourceGroupDeployment -Name ARMVMDeploy -ResourceGroupName PRASASRI -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployCloud.json
+New-AzureRmResourceGroup -Name PRASASRI-VS -Location 'East US' -Force
+New-AzureRmResourceGroupDeployment -Name ARMVMDeploy -ResourceGroupName PRASASRI-VS -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployCloud.json
+
+New-AzureRmResourceGroup -Name PRASASRI-WS -Location 'East US' -Force
+New-AzureRmResourceGroupDeployment -Name ARMVMDeploy -ResourceGroupName PRASASRI-Ws -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployUtility.json
--- Run.ps1
+++ Run.ps1
@@ -10,28 +10,31 @@ Function Execute-InstallationCommand([String] $CommandTitle, [String] $CommandPa
     "$(Get-Date) - Information - Completed $CommandTitle Installation."| Out-File C:\Configure.log -Append
 }
 
+## Create Software Folder Under C Drive
+New-Item -ItemType Directory C:\Software
+
 ## Install Hyper-V Feature required for Docker Installation.
 "$(Get-Date) - Information - Initiating Hyper-V Feature installation."| Out-File C:\Configure.log -Append
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 "$(Get-Date) - Information - Hyper-V Feature installation completed."| Out-File C:\Configure.log -Append
 
 ## Download Installer Files.
-Download-WebInstallerFile -FileTitle 'Docker for Windows (Docker Website)' -DestinationFile 'C:\Docker for Windows Installer.exe' -SourceFile 'https://download.docker.com/win/stable/Docker for Windows Installer.exe'
-Download-WebInstallerFile -FileTitle 'Visual Studio Code (Blob)' -DestinationFile 'C:\VSCodeSetup.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/VSCodeSetup-x64-1.29.1.exe?st=2018-12-10T04%3A04%3A40Z&se=2020-01-01T04%3A04%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=HcgbJvJkyCkrdRWSw57cZdgtkG40u70tt92EHyux374%3D'
-Download-WebInstallerFile -FileTitle 'Git (Blob)' -DestinationFile 'C:\Git.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/Git-2.19.1-64-bit.exe?st=2018-12-11T23%3A42%3A12Z&se=2020-12-30T23%3A42%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=NIGD9JpQw1U2L5GbTSHoFGpEs4jg8Fzwhgg7kvxDHE8%3D'
-Download-WebInstallerFile -FileTitle '7-Zip (7-Zip Website)' -DestinationFile 'C:\7z.msi' -SourceFile 'https://www.7-zip.org/a/7z1805-x64.msi'
-Download-WebInstallerFile -FileTitle 'Azure Data Studio (Blob)' -DestinationFile 'C:\AzureDataStudio.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/azuredatastudio-windows-setup-1.2.4.exe?st=2018-12-14T08%3A34%3A46Z&se=2020-12-31T08%3A34%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=hOKydqQvwEf5iSbWV4L1n7OrumvLezR5BAnua8rlij8%3D'
-Download-WebInstallerFile -FileTitle 'Azure Storage Explorer (Blob)' -DestinationFile 'C:\AzureStorageExplorer.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/StorageExplorer.exe?st=2018-12-14T08%3A35%3A11Z&se=2020-12-31T08%3A35%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=4IDNe7n1V6L8dW6%2BvZuTTTsIaJOCWMSjHSCYL36w%2FcY%3D'
-Download-WebInstallerFile -FileTitle 'Putty (Putty Website)' -DestinationFile 'C:\Putty.msi' -SourceFile 'https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi'
+Download-WebInstallerFile -FileTitle 'Docker for Windows (Docker Website)' -DestinationFile 'C:\Software\Docker for Windows Installer.exe' -SourceFile 'https://download.docker.com/win/stable/Docker for Windows Installer.exe'
+Download-WebInstallerFile -FileTitle 'Visual Studio Code (Blob)' -DestinationFile 'C:\Software\VSCodeSetup.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/VSCodeSetup-x64-1.29.1.exe?st=2018-12-10T04%3A04%3A40Z&se=2020-01-01T04%3A04%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=HcgbJvJkyCkrdRWSw57cZdgtkG40u70tt92EHyux374%3D'
+Download-WebInstallerFile -FileTitle 'Git (Blob)' -DestinationFile 'C:\Software\Git.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/Git-2.19.1-64-bit.exe?st=2018-12-11T23%3A42%3A12Z&se=2020-12-30T23%3A42%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=NIGD9JpQw1U2L5GbTSHoFGpEs4jg8Fzwhgg7kvxDHE8%3D'
+Download-WebInstallerFile -FileTitle '7-Zip (7-Zip Website)' -DestinationFile 'C:\Software\7z.msi' -SourceFile 'https://www.7-zip.org/a/7z1805-x64.msi'
+Download-WebInstallerFile -FileTitle 'Azure Data Studio (Blob)' -DestinationFile 'C:\Software\AzureDataStudio.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/azuredatastudio-windows-setup-1.2.4.exe?st=2018-12-14T08%3A34%3A46Z&se=2020-12-31T08%3A34%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=hOKydqQvwEf5iSbWV4L1n7OrumvLezR5BAnua8rlij8%3D'
+Download-WebInstallerFile -FileTitle 'Azure Storage Explorer (Blob)' -DestinationFile 'C:\Software\AzureStorageExplorer.exe' -SourceFile 'https://srinivasablobstorage.file.core.windows.net/downloads/StorageExplorer.exe?st=2018-12-14T08%3A35%3A11Z&se=2020-12-31T08%3A35%3A00Z&sp=rl&sv=2018-03-28&sr=f&sig=4IDNe7n1V6L8dW6%2BvZuTTTsIaJOCWMSjHSCYL36w%2FcY%3D'
+Download-WebInstallerFile -FileTitle 'Putty (Putty Website)' -DestinationFile 'C:\Software\Putty.msi' -SourceFile 'https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi'
 
 ## Run Installation commands.
-Execute-InstallationCommand -CommandTitle 'Docker for Windows' -CommandPath 'C:\Docker for Windows Installer.exe' -CommandArguments 'install --quiet'
-Execute-InstallationCommand -CommandTitle 'Visual Studio Code' -CommandPath 'C:\VSCodeSetup.exe' -CommandArguments '/VERYSILENT /MERGETASKS=!runcode'
-Execute-InstallationCommand -CommandTitle 'Git for Windows' -CommandPath 'C:\Git.exe' -CommandArguments '/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"'
-Execute-InstallationCommand -CommandTitle '7z' -CommandPath 'C:\Windows\System32\MsiExec.exe' -CommandArguments '/i C:\7z.msi /qn'
-Execute-InstallationCommand -CommandTitle 'Putty' -CommandPath 'C:\Windows\System32\MsiExec.exe' -CommandArguments '/i C:\Putty.msi /qn'
-Execute-InstallationCommand -CommandTitle 'Azure Data Studio' -CommandPath 'C:\AzureDataStudio.exe' -CommandArguments '/VERYSILENT /MERGETASKS=!runcode'
-Execute-InstallationCommand -CommandTitle 'Azure Storage Explorer' -CommandPath 'C:\AzureStorageExplorer.exe' -CommandArguments '/VERYSILENT /MERGETASKS=!runcode'
+Execute-InstallationCommand -CommandTitle 'Docker for Windows' -CommandPath 'C:\Software\Docker for Windows Installer.exe' -CommandArguments 'install --quiet'
+Execute-InstallationCommand -CommandTitle 'Visual Studio Code' -CommandPath 'C:\Software\VSCodeSetup.exe' -CommandArguments '/VERYSILENT /MERGETASKS=!runcode'
+Execute-InstallationCommand -CommandTitle 'Git for Windows' -CommandPath 'C:\Software\Git.exe' -CommandArguments '/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"'
+Execute-InstallationCommand -CommandTitle '7z' -CommandPath 'C:\Windows\System32\MsiExec.exe' -CommandArguments '/i C:\Software\7z.msi /qn'
+Execute-InstallationCommand -CommandTitle 'Putty' -CommandPath 'C:\Windows\System32\MsiExec.exe' -CommandArguments '/i C:\Software\Putty.msi /qn'
+Execute-InstallationCommand -CommandTitle 'Azure Data Studio' -CommandPath 'C:\Software\AzureDataStudio.exe' -CommandArguments '/VERYSILENT /MERGETASKS=!runcode'
+Execute-InstallationCommand -CommandTitle 'Azure Storage Explorer' -CommandPath 'C:\Software\AzureStorageExplorer.exe' -CommandArguments '/VERYSILENT /MERGETASKS=!runcode'
 
 ## Download AzureRm, AzureAD, GetPassword, BitBucket.v2, PackageManagement, Pester, Posh-Get, PowerShellGet PowerShell Modules.
 Install-Package AzureRm -AllowClobber -Force -Confirm:$False
