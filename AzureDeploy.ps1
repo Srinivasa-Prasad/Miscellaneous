7dbd5432270265e1e5af256563a886c9cf03b708
Srinivasa <srinivasa.prasad@anz.com>
2018-12-17 12:29:31 +0530

    Utility JSON Updated

--- AzureDeploy.ps1
+++ AzureDeploy.ps1
@@ -5,9 +5,9 @@ If((Get-AzureRmContext).Account.Id -ne 'srinivasa.prasad@hotmail.com'){
 }
 
 ## Deploy Infra
-New-AzureRmResourceGroup -Name SrinivasaLabs-Infra -Location 'East US' -Force
-New-AzureRmResourceGroupDeployment -Name ARMInfraDeploy -ResourceGroupName SrinivasaLabs-Infra -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployInfra.json
+## New-AzureRmResourceGroup -Name SrinivasaLabs-Infra -Location 'East US' -Force
+## New-AzureRmResourceGroupDeployment -Name ARMInfraDeploy -ResourceGroupName SrinivasaLabs-Infra -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployInfra.json
 
 ## Deploy VM
 New-AzureRmResourceGroup -Name PRASASRI -Location 'East US' -Force
-New-AzureRmResourceGroupDeployment -Name ARMVMDeploy -ResourceGroupName PRASASRI -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployVM.json
+New-AzureRmResourceGroupDeployment -Name ARMVMDeploy -ResourceGroupName PRASASRI -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/Miscellaneous/master/DeployCloud.json
